1ac1cfd4e13f70a59ed94f7c3977de5cc2f54a3d
Sakiyary <60864459+Sakiyary@users.noreply.github.com>
2022-04-14 22:55:39 +0800

    Have a test 2.

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello World! Have a test!'
 
 if __name__ == '__main__':
     app.run()
