faa9be051e29a9b749b20c64d0502c3985ae53f3
Sakiyary <60864459+Sakiyary@users.noreply.github.com>
2022-04-15 18:13:41 +0800

    try to use taro-ui-vue3 and success.

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,12 +1,28 @@
-import {createApp} from 'vue'
-import './app.scss'
+// import {createApp} from 'vue'
+// import './app.scss'
+//
+// import 'taro-ui/dist/style/index.scss'
+//
+// const App = createApp({
+//   onShow(options) {
+//   },
+//   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
+// })
+//
+// export default App
 
-import 'taro-ui/dist/style/index.scss'
+import {createApp} from 'vue'
+import {createUI} from 'taro-ui-vue3'
+// 引用全部组件样式
+import 'taro-ui-vue3/dist/style/index.scss'
 
 const App = createApp({
   onShow(options) {
   },
-  // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
+
+// 引用全部组件
+const tuv3 = createUI()
+App.use(tuv3)
 
 export default App
