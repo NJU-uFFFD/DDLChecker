80cfbad96a8fbaba25eef5a84265817766af1112
Sakiyary <60864459+Sakiyary@users.noreply.github.com>
2022-04-15 15:44:29 +0800

    try to cv taro-ui.

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,8 +1,11 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './app.scss'
 
+import 'taro-ui/dist/style/index.scss'
+
 const App = createApp({
-  onShow (options) {},
+  onShow(options) {
+  },
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
 
