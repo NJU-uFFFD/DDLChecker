e97b4a9b83f40c05f328d18435ae142cd8d5d574
Sakiyary <60864459+Sakiyary@users.noreply.github.com>
2022-04-26 00:52:36 +0800

    见README.md

--- frontend/src/components/card/account_card.vue
+++ frontend/src/components/card/account_card.vue
@@ -1,24 +1,27 @@
 <template>
-  <!--  <nut-cell-group title="account">-->
   <nut-cell
     class="account-card"
-    title="TEST CARD"
-    desc="设置">
+    :title=account_item.title
+    desc="查看详情"
+    @click="testClick">
     <template v-slot:icon>
       <img
         class="site-icon"
-        src="/assets/images/mooc.png"
+        :src=account_item.from
       />
     </template>
   </nut-cell>
-  <!--  </nut-cell-group>-->
 </template>
 
-<script>
+<script lang="ts">
 import {Toast} from '@nutui/nutui-taro';
+import {Account_List} from "../../types/Account_List";
 
 export default {
   name: "account-card",
+  props: {
+    account_item: Object as () => Account_List
+  },
   setup() {
     const testClick = (event) => {
       Toast.text('click');
@@ -32,7 +35,12 @@ export default {
 
 <style>
 .account-card {
-
+  align-items: center;
+  margin-left: 10px;
+  margin-top: 0;
+  margin-bottom: 5px;
+  height: 40px;
+  width: 95%;
   border-radius: 0;
   box-shadow: 0 3px 14px 0 rgba(237, 238, 241, 1);
 }
@@ -41,8 +49,7 @@ export default {
   width: 30px;
   height: 30px;
   margin-top: 2px;
-  margin-left: 10px;
-  margin-right: 20px;
+  margin-right: 16px;
 }
 
 </style>
