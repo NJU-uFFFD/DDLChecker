24e1414a8f4a06e9b81c50fb9cffcbee8b50e7ea
lyc8503 <36782264+lyc8503@users.noreply.github.com>
2022-04-26 11:57:04 +0800

    test call container.

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,10 +1,16 @@
 import {createApp} from 'vue'
 import {Button, Cell, CellGroup, Icon, Menu, MenuItem, Avatar} from "@nutui/nutui-taro";
 import './app.scss'
+import Taro from "@tarojs/taro";
 
 const App = createApp({
   onShow() {
   },
+})
+
+// 微信云托管初始化
+Taro.cloud.init({
+  env: "prod-8gf8jswafda304f9"
 })
 
 App.use(Button).use(Cell).use(CellGroup).use(Icon).use(Menu).use(MenuItem).use(Avatar)
