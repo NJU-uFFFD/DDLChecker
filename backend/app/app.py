e8d3e28740a9b7c9776be2bb1025d259babdaf96
Bob Huang <bc3104hzy@126.com>
2022-05-04 21:19:49 +0800

    Add db

--- backend/app/app.py
+++ backend/app/app.py
@@ -3,6 +3,10 @@ from flask import Flask
 # 所有的 @app.route 分类写到 routes 文件夹下的模块中
 from routes import *
 
+from config import *
+
+from db import db
+
 WX_APPID = ""
 
 
@@ -11,6 +15,11 @@ app.config['JSON_AS_ASCII'] = False
 app.register_blueprint(ddl.bp)
 app.register_blueprint(account.bp)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_ADDRESS}/{MYSQL_DATABASE}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db.init_app(app)
 
 @app.route("/")
 def hello():
--- backend/app/config.py
+++ backend/app/config.py
@@ -1,9 +1,10 @@
 import os
 
 # 默认配置
-MYSQL_ADDRESS = os.environ['MYSQL_ADDRESS'] or 'sh-cynosdbmysql-grp-e2da81to.sql.tencentcdb.com:27821'
-MYSQL_USERNAME = os.environ['MYSQL_USERNAME'] or 'root'
-MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD'] or 'g6jVKKbc'
+MYSQL_ADDRESS = os.environ.get('MYSQL_ADDRESS') or 'sh-cynosdbmysql-grp-e2da81to.sql.tencentcdb.com:27821'
+MYSQL_USERNAME = os.environ.get('MYSQL_USERNAME') or 'root'
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or 'g6jVKKbc'
+MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE') or 'ddl'
 
 APP_ID = ""
 APP_SECRET = ""
