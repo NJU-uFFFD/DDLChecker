b6b29c436362f90c47474afba57953c0f4115ad2
lyc8503 <lyc8503@foxmail.com>
2022-05-12 22:45:09 +0800

    前端封装 request, 便于测试

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,8 @@
+const USE_CONTAINER = false  // 使用容器服务
+const SERVER_ADDR = "http://127.0.0.1:5000"  // 不使用容器服务时的(测试用)地址
+const TEST_WX_OPENID = "114514"  // 本地测试时模拟的 OPENID
+
 const WX_SERVICE = 'flask-7dmy'
 const ENV_ID = 'prod-8gf8jswafda304f9'
 
-export {WX_SERVICE, ENV_ID}
+export {USE_CONTAINER, SERVER_ADDR, WX_SERVICE, ENV_ID, TEST_WX_OPENID}
