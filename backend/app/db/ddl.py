e534a5f77dd25c62a2f5c01176b6f8f56ec82165
LeoDu030314 <1667790372@qq.com>
2022-05-19 21:45:09 +0800

    modify requirement

--- backend/app/db/ddl.py
+++ backend/app/db/ddl.py
@@ -11,8 +11,9 @@ class Ddl(db.Model):
     tag: str
     course_uuid: str
     ddl_time: int
+    create_time: int
     is_completed: bool
-    source_uuid: str
+    platform_uuid: str
     id = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.Integer, db.ForeignKey('user.id'))
     title = db.Column(db.String(256))
@@ -20,15 +21,17 @@ class Ddl(db.Model):
     tag = db.Column(db.String(4096))
     course_uuid = db.Column(db.String(64))
     ddl_time = db.Column(db.BigInteger)
-    source_uuid = db.Column(db.String(64))
+    create_time = db.Column(db.BigInteger)
+    platform_uuid = db.Column(db.String(64))
     is_completed = db.Column(db.Boolean)
 
-    def __init__(self, userid, title, ddl_time, content, tag, course_uuid, source_uuid, is_completed=False):
+    def __init__(self, userid, title, ddl_time, create_time, content, tag, course_uuid, platform_uuid, is_completed=False):
         self.userid = userid
         self.title = title
         self.content = content
         self.tag = tag
         self.course_uuid = course_uuid
         self.ddl_time = ddl_time
-        self.source_uuid = source_uuid
+        self.create_time = create_time
+        self.platform_uuid = platform_uuid
         self.is_completed = is_completed
