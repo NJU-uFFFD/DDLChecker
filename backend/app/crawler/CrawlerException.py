dace091894426f98930895db44c7c89f1ce4f0a4
lyc8503 <36782264+lyc8503@users.noreply.github.com>
2022-05-21 17:51:46 +0800

    TeachingSquareCrawler ok.

--- backend/app/crawler/CrawlerException.py
+++ backend/app/crawler/CrawlerException.py
@@ -1,3 +1,8 @@
 class LoginException(Exception):
     def __init__(self, *args):
         super(LoginException, self).__init__(*args)
+
+
+class UnknownCrawlerException(Exception):
+    def __init__(self, *args):
+        super(UnknownCrawlerException, self).__init__(*args)
--- backend/app/crawler/util.py
+++ backend/app/crawler/util.py
@@ -1,7 +1,7 @@
-from crawler.crawlers.TeachingSquareCrawler import TeachingSquareCrawler
+from crawler.crawlers.TeachingSquareCrawler import TeachingSquareCrawler, TEACHING_SQUARE_CRAWLER_UUID
 
 
 def list_crawlers():
     return [
-        {"name": "教学立方", "uuid": "f15684f5-d870-4a9d-b859-e7eec3c6e3b5", "obj": TeachingSquareCrawler}
+        {"name": "教学立方", "uuid": TEACHING_SQUARE_CRAWLER_UUID, "obj": TeachingSquareCrawler}
     ]
