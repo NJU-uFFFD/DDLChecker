7b10e731de73ae8e22159c4f3fcc65535334849a
LeoDu030314 <1667790372@qq.com>
2022-05-22 01:31:42 +0800

    add 'ddl/update'

--- backend/app/routes/rules/ddl_rules.py
+++ backend/app/routes/rules/ddl_rules.py
@@ -4,9 +4,9 @@ from marshmallow import Schema, fields, validate
 
 class DeleteDDLRules(Schema):
     """
-        "ddl_id" -> int(>=0)
+        "id" -> int(>=0)
     """
-    ddl_id = fields.Integer(strict=True, required=True, validate=[validate.Range(min=0)])
+    id = fields.Integer(strict=True, required=True, validate=[validate.Range(min=0)])
 
 
 class ListDDLsRules(Schema):
@@ -16,6 +16,9 @@ class ListDDLsRules(Schema):
         "filter" -> dict(not necessary)
             "is_completed" -> bool
             "is_overtime" -> bool
+        "time_range" -> dict(not necessary)
+            "start" -> int
+            "end" -> int
         "tag" -> str(not necessary)
         "sorter" -> dict(not necessary)
             "reversed" -> bool
@@ -25,6 +28,9 @@ class ListDDLsRules(Schema):
     filter = fields.Dict(required=False,
                          keys=fields.Str(required=True, validate=validate.OneOf(["is_completed", "is_overtime"])),
                          values=fields.Boolean(required=True))
+    time_range = fields.Dict(required=False,
+                             keys=fields.Str(required=True, validate=validate.OneOf(["start", "end"])),
+                             values=fields.Integer(strict=True, required=True, validate=validate.Range(min=round(time.time() * 1000) - 2_592_000_000)))
     tag = fields.Str(required=False, validate=validate.Length(min=1, max=4096))
     sorter = fields.Dict(required=False,
                          keys=fields.Str(required=True, validate=validate.OneOf(["reversed"])),
@@ -42,8 +48,30 @@ class AddDDLRules(Schema):
     """
     title = fields.Str(required=True, validate=validate.Length(min=1, max=256))
     content = fields.Str(required=True, validate=validate.Length(min=1, max=4096))
-    ddl_time = fields.Integer(strict=True, required=True, validate=[validate.Range(min=round(time.time() * 1000) - 2_592_000_000)])
+    ddl_time = fields.Integer(strict=True, required=True, validate=validate.Range(min=round(time.time() * 1000) - 2_592_000_000))
     tag = fields.Str(required=False, validate=validate.Length(min=1, max=4096))
     course_uuid = fields.UUID(required=False)
     platform_uuid = fields.UUID(required=False)
+
+
+class UpdateDDLRules(Schema):
+    """
+        "id" -> int(>=0)
+        "title" -> str (len 1 - 256)(not necessary)
+        "content" -> str (len 1 - 4096)(not necessary)
+        "ddl_time" -> int(不得在30天前)(not necessary)
+        "tag" -> str(len 1 - 4096)(not necessary)
+        "course_uuid" -> str(not necessary)
+        "platform_uuid" -> str(not necessary)
+        "is_completed" -> bool(not necessary)
+    """
+    id = fields.Integer(strict=True, required=True, validate=[validate.Range(min=0)])
+    title = fields.Str(required=False, validate=validate.Length(min=1, max=256))
+    content = fields.Str(required=False, validate=validate.Length(min=1, max=4096))
+    ddl_time = fields.Integer(strict=True, required=False,
+                              validate=validate.Range(min=round(time.time() * 1000) - 2_592_000_000))
+    tag = fields.Str(required=False, validate=validate.Length(min=1, max=4096))
+    course_uuid = fields.UUID(required=False)
+    platform_uuid = fields.UUID(required=False)
+    is_completed = fields.Boolean(required=False)
 
