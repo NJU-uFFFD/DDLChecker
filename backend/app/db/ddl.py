5f3c85aa5167a816a1c1141c4f32a030e49347ff
LeoDu030314 <1667790372@qq.com>
2022-05-23 01:08:02 +0800

    add complete_time, is_deleted, delete_time coloum

--- backend/app/db/ddl.py
+++ backend/app/db/ddl.py
@@ -13,7 +13,11 @@ class Ddl(db.Model):
     ddl_time: int
     create_time: int
     is_completed: bool
+    complete_time: int
+    is_deleted: bool
+    delete_time: int
     platform_uuid: str
+    source_ddl_uuid: str
     id = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.Integer, db.ForeignKey('user.id'))
     title = db.Column(db.String(256))
@@ -24,8 +28,13 @@ class Ddl(db.Model):
     create_time = db.Column(db.BigInteger)
     platform_uuid = db.Column(db.String(64))
     is_completed = db.Column(db.Boolean)
+    is_deleted = db.Column(db.Boolean)
+    delete_time = db.Column(db.BigInteger)
+    complete_time = db.Column(db.BigInteger)
+    source_ddl_uuid = db.Column(db.String(64))
 
-    def __init__(self, userid, title, ddl_time, create_time, content, tag, course_uuid, platform_uuid, is_completed=False):
+    def __init__(self, userid, title, ddl_time, create_time, content, tag, course_uuid, platform_uuid,
+                 source_ddl_uuid=None, complete_time=None, delete_time=None, is_completed=False, is_deleted=False):
         self.userid = userid
         self.title = title
         self.content = content
@@ -35,3 +44,7 @@ class Ddl(db.Model):
         self.create_time = create_time
         self.platform_uuid = platform_uuid
         self.is_completed = is_completed
+        self.is_deleted = is_deleted
+        self.complete_time = complete_time
+        self.delete_time = delete_time
+        self.source_ddl_uuid = source_ddl_uuid
--- backend/app/routes/ddl.py
+++ backend/app/routes/ddl.py
@@ -5,7 +5,6 @@ from db import db
 from db.ddl import Ddl
 from db.user import User
 
-
 bp = Blueprint("ddl", __name__, url_prefix="/ddl")
 
 
@@ -24,7 +23,7 @@ def add_ddl():
     if user is None:
         return make_response(-1, "User not registered", {})
     userid = user.id
-    new_ddl = Ddl(userid, data['title'], data['ddl_time'], int(time.time()*1000), data['content'], data.get('tag'),
+    new_ddl = Ddl(userid, data['title'], data['ddl_time'], int(time.time() * 1000), data['content'], data.get('tag'),
                   data.get('course_uuid'), data.get('platform_uuid'))
     db.session.add(new_ddl)
     db.session.commit()
@@ -49,6 +48,9 @@ def list_dll():
                 "course_uuid" -> str
                 "platform_uuid" -> str
                 "is_completed" -> bool
+                "is_deleted" -> bool
+                "complete_time" -> int
+                "delete_time" -> int
             }
         ]
         "ddl_count" -> int
@@ -67,51 +69,36 @@ def list_dll():
     userid = user.id
     filter_list = [Ddl.userid == userid]
     if 'filter' in data:
-        if data['filter']['is_completed']:
-            filter_list.append(Ddl.is_completed is True)
-        if data['filter']['is_overtime']:
-            filter_list.append(Ddl.ddl_time < round(time.time() * 1000))
+        if 'is_completed' in data['filter']:
+            if data['filter']['is_completed']:
+                filter_list.append(Ddl.is_completed == True)
+            else:
+                filter_list.append(Ddl.is_completed == False)
+        if 'is_overtime' in data['filter']:
+            if data['filter']['is_overtime']:
+                filter_list.append(Ddl.ddl_time < round(time.time() * 1000))
+            else:
+                filter_list.append(Ddl.ddl_time >= round(time.time() * 1000))
+        if 'is_deleted' in data['filter']:
+            if data['filter']['is_deleted']:
+                filter_list.append(Ddl.is_deleted == True)
     if 'tag' in data:
         filter_list.append(Ddl.tag == data['tag'])
     if 'time_range' in data:
-        if data['time_range']['start'] > data['time_range']['end']:
-            return make_response(400, "Invalid time range.(nmsl)", {})
-        else:
-            filter_list.append(data['time_range']['start'] <= Ddl.ddl_time)
-            filter_list.append(data['time_range']['end'] >= Ddl.ddl_time)
-    ddl_count = len(Ddl.query.filter(*filter_list).order_by(Ddl.ddl_time.desc() if 'sorter' in data and data['sorter']['reversed']
+        if 'start' in data['time_range'] and 'end' in data['time_range']:
+            if data['time_range']['start'] > data['time_range']['end']:
+                return make_response(400, "Invalid time range.(nmsl)", {})
+            else:
+                filter_list.append(data['time_range']['start'] <= Ddl.ddl_time)
+                filter_list.append(data['time_range']['end'] >= Ddl.ddl_time)
+    ddl_count = len(
+        Ddl.query.filter(*filter_list).order_by(Ddl.ddl_time.desc() if 'sorter' in data and data['sorter']['reversed']
                                                 else Ddl.ddl_time).all())
     return make_response(0, "OK", {'ddl_list': (Ddl.query.filter(*filter_list).
-                                                order_by(Ddl.ddl_time.desc() if 'sorter' in data and data['sorter']['reversed']
-                                                else Ddl.ddl_time).slice(data['start'], data['end']).all()),
+                                                order_by(
+        Ddl.ddl_time.desc() if 'sorter' in data and data['sorter']['reversed']
+        else Ddl.ddl_time).slice(data['start'], data['end']).all()),
                                    'ddl_count': ddl_count})
-
-
-@bp.route("/delete", methods=['POST', 'GET'])
-def delete_ddl():
-    """
-    接受ddl删除请求，删除ddl
-    :return: make_response()
-    return_data = {"id" -> int(>=0)}
-    """
-    openid, data = get_context()
-    check_data(DeleteDDLRules, data)
-
-    # 数据库操作
-    user = User.query.filter(User.openid == openid).first()
-    if user is None:
-        return make_response(-1, "User not registered.", {})
-    userid = user.id
-    ddl = Ddl.query.get(data['id'])
-    if ddl is None:
-        return make_response(404, "Id not found.(nmsl)", {})
-    if ddl.userid != userid:
-        return make_response(403, "Cannot delete others' ddl.(nmsl)", {})
-
-    db.session.delete(ddl)
-    db.session.commit()
-
-    return make_response(0, "OK", {"id": ddl.id})
 
 
 @bp.route("/update", methods=['POST', 'GET'])
@@ -149,8 +136,16 @@ def update_ddl():
         ddl.platform_uuid = data['platform_uuid']
     if 'is_completed' in data:
         ddl.is_completed = data['is_completed']
-
+        if data['is_completed']:
+            ddl.complete_time = round(time.time() * 1000)
+        else:
+            ddl.complete_time = None
+    if 'is_deleted' in data:
+        ddl.is_deleted = data['is_deleted']
+        if data['is_deleted']:
+            ddl.delete_time = round(time.time() * 1000)
+        else:
+            ddl.delete_time = None
     db.session.commit()
 
     return make_response(0, "OK", {"id": ddl.id})
-
