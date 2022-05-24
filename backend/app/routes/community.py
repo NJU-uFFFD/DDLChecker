51340f325a574938c45f262b83683d6ad00c46d3
LeoDu030314 <1667790372@qq.com>
2022-05-24 17:00:21 +0800

    add course_table and source_ddl_table.

--- backend/app/routes/community.py
+++ backend/app/routes/community.py
@@ -1,8 +1,12 @@
 from flask import Blueprint
 from routes.utils import get_context, check_data, make_response
+from db.course import Course
+from db.source_ddl import SourceDdl
 
 
 bp = Blueprint("community", __name__, url_prefix="/community")
+
+
 
 
 
