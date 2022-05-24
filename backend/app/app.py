ae5192d2375938ffd56036957eb54ec26891bcb7
lyc8503 <36782264+lyc8503@users.noreply.github.com>
2022-05-24 15:33:39 +0800

    update aliyun invoke.

--- backend/app/app.py
+++ backend/app/app.py
@@ -6,6 +6,9 @@ import logging
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+from flask import request
+import service
+
 WX_APPID = ""
 
 logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s', level=logging.INFO)
@@ -15,7 +18,6 @@ app.register_blueprint(ddl.bp)
 app.register_blueprint(account.bp)
 app.register_blueprint(user.bp)
 app.register_blueprint(community.bp)
-app.register_blueprint(cron.bp)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_ADDRESS}/{MYSQL_DATABASE}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -38,6 +40,12 @@ limiter = Limiter(
 @app.route("/")
 def hello():
     return "hello!"
+
+
+@app.route('/invoke')
+def invoke():
+    assert request.get_json()['cron_token'] == INVOKE_PATH_TOKEN, "Invalid cron token."
+    service.cron.cron_work()
 
 
 if __name__ == '__main__':
