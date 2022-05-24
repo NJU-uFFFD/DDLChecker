6aacad7b0d5ea64259184d9afbe766be09a139af
LeoDu030314 <1667790372@qq.com>
2022-05-24 23:36:43 +0800

    create table Account.

--- backend/app/routes/community.py
+++ backend/app/routes/community.py
@@ -3,6 +3,7 @@ from routes.utils import get_context, check_data, make_response
 from db.userSubs import UserSubscriptions
 from db.sourceDdl import SourceDdl
 from db.course import Course
+from db.account import Account
 
 
 bp = Blueprint("community", __name__, url_prefix="/community")
