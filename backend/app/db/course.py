d54f98ea271f428603f904306e4b0042a6891cf6
LeoDu030314 <1667790372@qq.com>
2022-05-24 17:00:21 +0800

    add course_table and source_ddl_table.

--- backend/app/db/course.py
+++ backend/app/db/course.py
@@ -10,7 +10,7 @@ class Course(db.Model):
     platform_uuid: str
     id = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.Integer, db.ForeignKey('user.id'))
-    course_uuid = db.Column(db.String(64))
+    course_uuid = db.Column(db.String(64), unique=True)
     platform_uuid = db.Column(db.String(64))
 
     def __init__(self, userid, course_uuid, platform_uuid):
