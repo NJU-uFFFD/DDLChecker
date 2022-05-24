ececb0187436dd4f8341af9d890d903a47c74ed2
Bob Huang <bc3104hzy@126.com>
2022-05-24 18:03:57 +0800

    add some backrefs

--- backend/app/db/course.py
+++ backend/app/db/course.py
@@ -8,7 +8,9 @@ class Course(db.Model):
     platform_uuid: str
     course_uuid = db.Column(db.String(64), primary_key=True)
     platform_uuid = db.Column(db.String(64))
-
+    ddls = db.relationship('Ddl', backref='course', lazy='dynamic')
+    source_ddls = db.relationship('SourceDdl', backref='course', lazy='dynamic')
+    subs = db.relationship('UserSubscriptions', backref='course', lazy='dynamic')
 
 def __init__(self, course_uuid, platform_uuid):
     self.course_uuid = course_uuid
