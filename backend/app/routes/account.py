2e208da578f1eea2cf8945505bd2118d13def913
lyc8503 <lyc8503@foxmail.com>
2022-05-26 11:11:36 +0800

    account list

--- backend/app/routes/account.py
+++ backend/app/routes/account.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 from crawler.util import list_crawlers
-from routes.utils import get_context, make_response
+from routes.utils import get_context_user, make_response
 from db.account import Account
 
 bp = Blueprint("account", __name__, url_prefix="/account")
@@ -9,30 +9,34 @@ bp = Blueprint("account", __name__, url_prefix="/account")
 
 @bp.route("/available", methods=['POST', 'GET'])
 def available_account_type():
-    data, open_id = get_context()
+    user, data = get_context_user(data_required=False)
+    current_accounts = user.accounts.all()
     tmp = []
     for c in list_crawlers():
-        tmp.append({"name": c['name'], "uuid": c['uuid'], "fields": c['obj'].required_fields()})
+        bound = True in [c['uuid'] == account.platform_uuid for account in current_accounts]
+        tmp.append({"name": c['name'], "uuid": c['uuid'], "fields": c['obj'].required_fields(), "bound": bound})
     return make_response(0, "OK", {"available_account_type": tmp})
 
 
 @bp.route("/add", methods=['POST', 'GET'])
 def add_account():
-    data, open_id = get_context()
+    user, data = get_context_user()
+
+    # 检查 account 是否存在
 
     return make_response(0, "OK", {})
 
 
 @bp.route("/list", methods=['POST', 'GET'])
 def list_account():
-    data, open_id = get_context()
+    user, data = get_context_user()
 
     return
 
 
 @bp.route("/delete", methods=['POST', 'GET'])
 def delete_account():
-    data, open_id = get_context()
+    user, data = get_context_user()
 
     return
 
