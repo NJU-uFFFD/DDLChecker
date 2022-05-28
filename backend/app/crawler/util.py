48807daa40040744b39d83128224d1df8e3a541c
lyc8503 <lyc8503@foxmail.com>
2022-05-28 22:38:14 +0800

    add attr crawler check

--- backend/app/crawler/util.py
+++ backend/app/crawler/util.py
@@ -5,6 +5,6 @@ from crawler.crawlers.NjuSpocCrawler import NJU_SPOC_UUID, NjuSpocCrawler
 
 def list_crawlers():
     return [
-        {"name": "教学立方", "uuid": TEACHING_SQUARE_CRAWLER_UUID, "obj": TeachingSquareCrawler},
-        {"name": "南大SPOC", "uuid": NJU_SPOC_UUID, "obj": NjuSpocCrawler}
+        {"name": "教学立方", "uuid": TEACHING_SQUARE_CRAWLER_UUID, "obj": TeachingSquareCrawler, "check": True},
+        {"name": "南大SPOC", "uuid": NJU_SPOC_UUID, "obj": NjuSpocCrawler, "check": False}
     ]
