c7a21c2c829ff4747cb380113e2e186bf87f9aa0
LeoDu030314 <1667790372@qq.com>
2022-05-30 11:40:42 +0800

    I start to write unitest

---
+++ backend/test/test_account.py
@@ -0,0 +1,21 @@
+import unittest
+
+class TestAccount(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_add_account(self):
+        pass
+
+    def test_list_account(self):
+        pass
+
+    def test_delete_account(self):
+        pass
+
+    def test_available_account_type(self):
+        pass
+
