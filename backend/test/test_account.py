9e0701cc80ea7719b290b5d3f4ad7a15bd49955d
LeoDu030314 <1667790372@qq.com>
2022-05-30 18:04:41 +0800

    Add unittest for account.

--- backend/test/test_account.py
+++ backend/test/test_account.py
@@ -1,21 +1,152 @@
 import unittest
 
+from app import app
+from db import db
+from db.account import Account
+from db.ddl import Ddl
+from db.user import User
+from db.course import Course
+import json
+import time
+import uuid
+
+from util.encrypt import aes_encrypt
+
 class TestAccount(unittest.TestCase):
+
     def setUp(self):
-        pass
+        self.app = app
+        self.client = self.app.test_client()
+        with self.app.app_context():
+            db.create_all()
+            user = User('114514', '田所浩二')
+            user2 = User('1919810', '田所浩三')
+            db.session.add(user)
+            db.session.add(user2)
+            db.session.commit()
 
     def tearDown(self):
-        pass
+        with self.app.app_context():
+            db.drop_all()
+
+    def test_available_account(self):
+        ret = self.client.post("/account/available", headers={
+            'x-wx-source': 'test',
+            'x-wx-openid': '114514'
+        })
+        self.assertEqual(ret.status_code, 200)
+        ret = json.loads(ret.data.decode("utf-8"))
+        self.assertEqual(ret['code'], 0)
+        self.assertIsNotNone(ret['data'].get("available_account_type"))
 
     def test_add_account(self):
-        pass
+        with self.app.app_context():
+            ret = self.client.post("/account/add", headers={
+                'x-wx-source': 'test',
+                'x-wx-openid': '114514'
+            }, json={
+                'platform_uuid': '68dc1014-7bfe-4ea3-a000-5734303d9f59',
+                'fields': {"account": "田所浩二", "password": "nmsl"}
+            })
+            self.assertEqual(ret.status_code, 200)
+            ret = json.loads(ret.data.decode("utf-8"))
+            self.assertEqual(ret['code'], 0)
+            f = Account.query.filter(Account.id == ret['data']['id']).first().fields
+            self.assertEqual(f, aes_encrypt(json.dumps({"account": "田所浩二", "password": "nmsl"})))
+
+    def test_add_account_nonexist_platform(self):
+        with self.app.app_context():
+            ret = self.client.post("/account/add", headers={
+                'x-wx-source': 'test',
+                'x-wx-openid': '114514'
+            }, json={
+                'platform_uuid': str(uuid.uuid4()),
+                'fields': {"account": "田所浩三", "password": "nmysl"}
+            })
+            self.assertEqual(ret.status_code, 200)
+            ret = json.loads(ret.data.decode("utf-8"))
+            self.assertEqual(-1, ret['code'])
+            self.assertIn("Invalid platform_uuid", ret['msg'])
+            self.assertEqual(Account.query.count(), 0)
+
+    def test_add_account_same_platform(self):
+        with self.app.app_context():
+            account = Account("1", '68dc1014-7bfe-4ea3-a000-5734303d9f59', aes_encrypt(json.dumps({"account": "田所浩二", "password": "nmsl"})))
+            db.session.add(account)
+            db.session.commit()
+            ret = self.client.post("/account/add", headers={
+                'x-wx-source': 'test',
+                'x-wx-openid': '114514'
+            }, json={
+                'platform_uuid': '68dc1014-7bfe-4ea3-a000-5734303d9f59',
+                'fields': {"account": "田所浩三", "password": "nmysl"}
+            })
+            self.assertEqual(ret.status_code, 200)
+            ret = json.loads(ret.data.decode("utf-8"))
+            self.assertEqual(-1, ret['code'])
+            self.assertIn("more than one", ret['msg'])
+            self.assertEqual(Account.query.count(), 1)
+
+    def test_too_many_account(self):
+        with self.app.app_context():
+            for _ in range(0, 29):
+                ret1 = self.client.post("/account/add", headers={
+                    'x-wx-source': 'test',
+                    'x-wx-openid': '114514'
+                }, json={
+                    'platform_uuid': '68dc1014-7bfe-4ea3-a000-5734303d9f59',
+                    'fields': {"account": "田所浩二", "password": "nmsl"}
+                })
+                ret2 = self.client.post("/account/delete", headers={
+                    'x-wx-source': 'test',
+                    'x-wx-openid': '114514'
+                }, json={
+                    'id': json.loads(ret1.data.decode("utf-8"))['data']['id']
+                })
+            ret1 = self.client.post("/account/add", headers={
+                'x-wx-source': 'test',
+                'x-wx-openid': '114514'
+            }, json={
+                'platform_uuid': '68dc1014-7bfe-4ea3-a000-5734303d9f59',
+                'fields': {"account": "田所浩二", "password": "nmsl"}
+            })
+            self.assertEqual(ret1.status_code, 200)
+            ret1 = json.loads(ret1.data.decode("utf-8"))
+            self.assertEqual(-1, ret1['code'])
+            self.assertIn("times limitation", ret1['msg'])
+            self.assertEqual(Account.query.count(), 0)
 
     def test_list_account(self):
-        pass
+        with self.app.app_context():
+            account = Account("1", '68dc1014-7bfe-4ea3-a000-5734303d9f59', aes_encrypt(json.dumps({"account": "田所浩二", "password": "nmsl"})))
+            db.session.add(account)
+            account = Account("2", '68dc1014-7bfe-4ea3-a000-5734303d9f59', aes_encrypt(json.dumps({"account": "田所浩三", "password": "nmysl"})))
+            db.session.add(account)
+            db.session.commit()
+            ret = self.client.post("/account/list", headers={
+                'x-wx-source': 'test',
+                'x-wx-openid': '114514'
+            })
+            self.assertEqual(ret.status_code, 200)
+            ret = json.loads(ret.data.decode("utf-8"))
+            self.assertEqual(ret['code'], 0)
+            self.assertEqual(ret['data']['account_count'], 1)
 
     def test_delete_account(self):
-        pass
-
-    def test_available_account_type(self):
-        pass
-
+        with self.app.app_context():
+            account = Account("1", '68dc1014-7bfe-4ea3-a000-5734303d9f59', aes_encrypt(json.dumps({"account": "田所浩二", "password": "nmsl"})))
+            db.session.add(account)
+            account = Account("2", '68dc1014-7bfe-4ea3-a000-5734303d9f59', aes_encrypt(json.dumps({"account": "田所浩三", "password": "nmysl"})))
+            db.session.add(account)
+            db.session.commit()
+            ret = self.client.post("/account/list", headers={
+                'x-wx-source': 'test',
+                'x-wx-openid': '114514'
+            }, json={
+                "id": 1
+            })
+            self.assertEqual(ret.status_code, 200)
+            ret = json.loads(ret.data.decode("utf-8"))
+            self.assertEqual(ret['code'], 0)
+            self.assertIsNone(Account.query.filter(Account.id == 1).first())
+            self.assertIsNotNone(Account.query.filter(Account.id == 2).first())
