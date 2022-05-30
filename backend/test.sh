3a34748f8799264f4d7d63aef507d4d800af713e
LeoDu030314 <1667790372@qq.com>
2022-05-30 20:56:55 +0800

    avatar limitation to 30.

--- backend/test.sh
+++ backend/test.sh
@@ -1,3 +1,3 @@
 MYSQL_ADDRESS=114.212.102.31 MYSQL_DATABASE=test233 MYSQL_PASSWORD=Dlx0314 MYSQL_USERNAME=test coverage run -m unittest discover
-coverage report -m
+coverage report -m src/routes/*.py
 sleep 1145141919810s
